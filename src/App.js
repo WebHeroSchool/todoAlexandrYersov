b5b15b649c052dabfe08aec9cf31b45c423985b2
Александр Ершов <sanya_evr@mail.ru>
2021-04-19 17:19:29 +0600

    lesson 16: starting from the beginning

--- src/App.js
+++ src/App.js
@@ -1,56 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
-import { count, length } from './numbers.js';
+import React from 'react';
 
-const name = 'Alexandr';
-const age = 34;
-const human = true;
-const flag = false;
+const element = (<div>
+  Hello world!
+  <a href='#'> Go to </a>
+</div>);
 
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p style={{
-          color: 'red'
-        }}>
-          Имя {name}, возраст {age}, {human && 'человек'}
-        </p>
-        <p style={{
-          fontSize: 16+2,
-          color: 'green'
-        }}>
-          2 + 2 = {2 + 2}
-        </p>
-        <p>
-          {flag ? 'Flag is true' : 'Flag is false'}
-          {undefined}
-          {null}
-          {true}
-          {false}
-        </p>
-        <p style={{
-          border: '3px solid red',
-          padding: '0 15px',
-        }}>
-          count * length = { count * length }
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+export { element };
