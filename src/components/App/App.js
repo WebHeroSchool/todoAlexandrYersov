fefe99abce75d3c1be170d30a40b830fe19c32c0
Alexandr Yershov <69073713+Alexandr886@users.noreply.github.com>
2021-04-25 21:46:54 +0600

    lesson 21: Вывод списка элементов и ключи

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,13 +3,25 @@ import InputItem from '../InputItem/InputItem.js'
 import ItemList from '../ItemList/ItemList.js';
 import Footer from '../Footer/Footer.js';
 
-const todoItem = 'Написать приложение';
-const App = () => (
-  <div>
-    <h1> Важные дела </h1>
-    <InputItem />
-    <ItemList todoItem={todoItem} />
-    <Footer count={3} />
+const App = () => {
+    const items = [
+        {
+            value: 'Написать приложение'
+        },
+        {
+            value: 'Прописать props'
+        },
+        {
+            value: 'Сделать все дела'
+        },
+    ];
+    return (
+    <div>
+        <h1> Важные дела </h1>
+        <InputItem />
+        <ItemList items={items} />
+        <Footer count={3} />
     </div>);
+}
 
 export default App;
--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import Item from '../Item/Item.js';
 
-const ItemList = ({ todoItem }) => (<ul>
-  <li> <Item todoItem={todoItem} /> </li>
-  <li> <Item todoItem={'Прописать props'} /> </li>
-  <li> <Item todoItem={'Сделать все дела'} /> </li>
+const ItemList = ({ items }) => (<ul>
+    {items.map(item => <li key={item.value}><Item value={item.value}  /></li>)}
 </ul>)
 
 export default ItemList;
